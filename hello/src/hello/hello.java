c0a60960ece62382a3ca101242fadc2f98cdcc5f
Alexander Millett <alexandermillett@alexanders-air.lan>
2020-02-02 21:51:26 -0500

    goodbye message

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -4,8 +4,9 @@ public class hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Here ye : important announcement");
+		System.out.println("Here ye : important announcement.");
 		System.out.println("Hello World");
+		System.out.println("See you all in the future! Goodbye.");
 	}
 
 }
