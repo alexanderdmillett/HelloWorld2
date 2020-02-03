d70a1fdc247ecc098b4750f4f981641526bc1da9
Alexander Millett <alexandermillett@alexanders-air.lan>
2020-02-02 21:49:11 -0500

    added here ye announcement

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -3,7 +3,8 @@ package hello;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World");
 	}
 
